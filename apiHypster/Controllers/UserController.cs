d4c1c97b5bbea7491afa12bd2188ca3b1db2b120
Richard Wu <richardw@junemedia.com>
2016-12-13 15:15:12 -0500

    1. The PUT method is added: public responseData putUser([FromUri] int userId, [FromBody] memberUser user).
    2. The memberUser object has been revised by adding more attributes into it.
    
    3. The Resource object has been revised by adding 2 methods: getQueryStream and padData.
    
    4. Deployed the api.hypster.com on New Prod Server.

--- apiHypster/Controllers/UserController.cs
+++ apiHypster/Controllers/UserController.cs
@@ -1,11 +1,6 @@
 ﻿using apiHypster.Models;
-using Newtonsoft.Json;
+using hypster_tv_DAL;
 using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Runtime.Serialization;
 using System.Web;
 using System.Web.Http;
 
@@ -14,23 +9,25 @@ namespace apiHypster.Controllers
     public class UserController : ApiController
     {
         public UserController() { }
-        hypster_tv_DAL.memberManagement memberManager = new hypster_tv_DAL.memberManagement();
-        hypster_tv_DAL.Member member = new hypster_tv_DAL.Member();
+        memberManagement memberManager = new memberManagement();
+        Member member = new Member();
         Resources res = new Resources();
-        //bool test = false;
+        //bool test = true;
         [Route("user/validate")]
         [HttpGet]
         public responseData validate()
         {
-            responseData obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "API error: The requested resource does not support http method 'GET'." }; ;
+            responseData obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "API error: The requested resource does not support http method 'GET'." };
             return obj;
         }
         [Route("user/validate")]
         [HttpPost]
         public responseData validate([FromBody] User user)
         {
-            HttpRequest httpReq = HttpContext.Current.Request;            
+            HttpRequest httpReq = HttpContext.Current.Request;
+            HttpResponse httpRes = HttpContext.Current.Response;
             responseData obj = new responseData();
+            memberUser mem = new memberUser();
             string clientIp = res.GetUserIP();
             //if (test?true:res.checkIpAddr(clientIp))
             if (res.checkIpAddr(clientIp))
@@ -47,17 +44,7 @@ namespace apiHypster.Controllers
                             obj = new responseData { status = (int)Resources.xhrCode.INVALID, message = "Invalid Password" };
                         else
                         {
-                            memberUser mem = new memberUser {
-                                id = member.id,
-                                username = member.username,
-                                email = member.email,
-                                name = member.name,
-                                country = (member.country != "" ? member.country.ToLower() : null),
-                                city = (member.city != "" ? member.city.ToLower() : null),
-                                zipcode = (member.zipcode != "" ? member.zipcode.ToLower() : null),
-                                birth = Convert.ToDateTime(member.birth).ToString("MM/dd/yyyy"),
-                                adminlevel = member.adminLevel
-                            };
+                            mem = res.padData(member, new memberUser());
                             obj = new responseData { status = (int)Resources.xhrCode.OK, message = "Validation Successful", user = mem };
                         }
                     }
@@ -66,23 +53,15 @@ namespace apiHypster.Controllers
                 }
                 catch (Exception e)
                 {
-                    HttpResponse httpRes = HttpContext.Current.Response;
+                    obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = e.ToString() };
                     httpRes.Status = "500 Internal Server Error";
                     httpRes.StatusCode = 500;
-                    httpRes.StatusDescription = "Internal Server Error during the process";
-                    httpRes.AddHeader("ClientIPAddr", clientIp);
-                    httpRes.AddHeader("Status", "500 Internal Server Error");
-                    httpRes.AddHeader("StatusCode", "500");
-                    httpRes.AddHeader("InnerException", e.InnerException.ToString());
-                    httpRes.AddHeader("Message", e.Message);
-                    httpRes.AddHeader("StackTrace", e.StackTrace);
+                    httpRes.StatusDescription = "500 ISE: General Exception";
                     httpRes.Flush();
-                    obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "Internal Server Error during the process" };
                 }
             }
             else
             {
-                HttpResponse httpRes = HttpContext.Current.Response;
                 httpRes.ClearHeaders();
                 httpRes.Status = "403 Forbidden";
                 httpRes.StatusCode = 403;
@@ -102,6 +81,7 @@ namespace apiHypster.Controllers
         {
             HttpResponse httpRes = HttpContext.Current.Response;
             responseData obj = new responseData();
+            memberUser mem = new memberUser();
             string clientIp = res.GetUserIP();
             //if (test?true:res.checkIpAddr(clientIp))
             if (res.checkIpAddr(clientIp))
@@ -112,18 +92,7 @@ namespace apiHypster.Controllers
                     member = memberManager.getMemberByID(userId);
                     if (member.id != 0)
                     {
-                        memberUser mem = new memberUser
-                        {
-                            id = member.id,
-                            username = member.username,
-                            email = member.email,
-                            name = member.name,
-                            country = (member.country!=""?member.country.ToLower() : null),
-                            city = (member.city != "" ? member.city.ToLower() : null),
-                            zipcode = (member.zipcode != "" ? member.zipcode.ToLower() : null),
-                            birth = Convert.ToDateTime(member.birth).ToString("MM/dd/yyyy"),
-                            adminlevel = member.adminLevel
-                        };
+                        mem = res.padData(member, new memberUser());
                         obj = new responseData { status = (int)Resources.xhrCode.OK, message = "User Found", user = mem };
                     }
                     else
@@ -131,16 +100,95 @@ namespace apiHypster.Controllers
                 }
                 catch (Exception e)
                 {
-                    obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "Internal Server Error during the process" };
+                    obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = e.ToString() };
                     httpRes.Status = "500 Internal Server Error";
                     httpRes.StatusCode = 500;
                     httpRes.StatusDescription = "Internal Server Error during the process";
-                    httpRes.AddHeader("ClientIPAddr", clientIp);
-                    httpRes.AddHeader("Status", "500 Internal Server Error");
-                    httpRes.AddHeader("StatusCode", "500");
-                    httpRes.AddHeader("InnerException", e.InnerException.ToString());
-                    httpRes.AddHeader("Message", e.Message);
-                    httpRes.AddHeader("StackTrace", e.StackTrace);
+                    httpRes.Flush();
+                }
+            }
+            else
+            {
+                obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "Unauthorized Location from the IP Address: " + clientIp };
+                httpRes.ClearHeaders();
+                httpRes.Status = "403 Forbidden";
+                httpRes.StatusCode = 403;
+                httpRes.StatusDescription = "Unauthorized Location";
+                httpRes.AddHeader("ClientIPAddr", clientIp);
+                httpRes.AddHeader("Status", "403 Forbidden");
+                httpRes.AddHeader("StatusCode", "403");
+                httpRes.AddHeader("StatusDescription", "Unauthorized Location from the IP Address: " + clientIp);
+                httpRes.Flush();
+            }
+            return obj;
+        }
+        [Route("user/{userId:int}")]
+        [HttpPut]
+        public responseData putUser([FromUri] int userId, [FromBody] memberUser user)
+        {
+            HttpRequest httpReq = HttpContext.Current.Request;
+            HttpResponse httpRes = HttpContext.Current.Response;
+            memberUser mem = new memberUser();
+            responseData obj = new responseData();
+            string clientIp = res.GetUserIP();
+            string querystream = "";
+            string message = "";
+            //if (test?true:res.checkIpAddr(clientIp))
+            if (res.checkIpAddr(clientIp))
+            {
+                try
+                {
+                    member = memberManager.getMemberByID(userId);
+                    if (member.id != 0)
+                    {                        
+                        if (user != null)
+                        {
+                            HttpContext.Current.Response.AddHeader("ClientIPAddr", clientIp);
+                            user.id = member.id;
+                            mem = res.padData(member, user);
+                            querystream = res.getQueryStream(member, mem);
+                            if (querystream != "")
+                            {
+                                memberManager.UpdateMemberData(user.id, querystream);
+                                member = memberManager.getMemberByID(userId);
+                                mem = res.padData(member, user);
+                                message = "User Updated";
+                            }
+                            else
+                                message = "User Found Without Update";
+                        }
+                        else
+                        {
+                            mem = res.padData(member, new memberUser());
+                            message = "User Found";
+                        }
+                        obj = new responseData { status = (int)Resources.xhrCode.OK, message = message, user = mem };
+                    }
+                    else
+                        obj = new responseData { status = (int)Resources.xhrCode.NOT_FOUND, message = "User Not Found" };
+                }
+                catch (ArgumentNullException ane)
+                {
+                    obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = ane.ToString() };
+                    httpRes.Status = "500 Internal Server Error";
+                    httpRes.StatusCode = 500;
+                    httpRes.StatusDescription = "500 ISE: ArgumentNullException";
+                    httpRes.Flush();
+                }
+                catch (FormatException fe)
+                {
+                    obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = fe.ToString() };
+                    httpRes.Status = "500 Internal Server Error";
+                    httpRes.StatusCode = 500;
+                    httpRes.StatusDescription = "500 ISE: FormatException";
+                    httpRes.Flush();
+                }
+                catch (Exception e)
+                {
+                    obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = e.ToString() };
+                    httpRes.Status = "500 Internal Server Error";
+                    httpRes.StatusCode = 500;
+                    httpRes.StatusDescription = "Internal Server Error during the process";
                     httpRes.Flush();
                 }
             }
--- apiHypster/Models/memberUser.cs
+++ apiHypster/Models/memberUser.cs
@@ -1,19 +1,39 @@
-﻿using System;
-using Newtonsoft.Json;
-
-namespace apiHypster.Models
+﻿namespace apiHypster.Models
 {
     public class memberUser
     {
         public int id { get; set; }
         public string username { get; set; }
-        public string email { get; set; }
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        //public string password { get; set; }
         public string name { get; set; }
-        public int adminlevel { get; set; }
-        public string country { get; set; }
+        public string email { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string address { get; set; }
         public string city { get; set; }
+        public string state { get; set; }
         public string zipcode { get; set; }
+        public string country { get; set; }
         public string birth { get; set; }
+        public string introduce { get; set; }
+        public int? sex { get; set; }
+        public int? trackcount { get; set; }
+        public int? israndomplay { get; set; }
+        public int? active_playlist { get; set; }
+        public int? profile_pic_id { get; set; }
+        public int? is_new_artist { get; set; }
+        public int? autoplay { get; set; }
+        public int? email_confirmed { get; set; }
+        public int? email_optout { get; set; }
+        public int? widget_views { get; set; }
+        public int? coreg_processed { get; set; }
+        public int? status_id { get; set; }
+        public int? autoshare_enabled { get; set; }
+        public int? artist_level { get; set; }
+        public int? is_over_eighteen { get; set; }
+        public int? user_interest { get; set; }
+        public int? e_camp_d { get; set; }
+        public int? adminlevel { get; set; }
+        public int? is_suspended { get; set; }
     }
 }
