d4e1458c4bcb048930f7972bfe9aa26c00181720
Richard Wu <richardw@junemedia.com>
2016-12-13 15:15:12 -0500

    1. The PUT method is added: public responseData putUser([FromUri] int userId, [FromBody] memberUser user).
    2. The memberUser object has been revised by adding more attributes into it.
    
    3. The Resource object has been revised by adding 2 methods: getQueryStream and padData.

--- apiHypster/Controllers/UserController.cs
+++ apiHypster/Controllers/UserController.cs
@@ -1,11 +1,6 @@
 ﻿using apiHypster.Models;
-using Newtonsoft.Json;
+using hypster_tv_DAL;
 using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Runtime.Serialization;
 using System.Web;
 using System.Web.Http;
 
@@ -14,23 +9,24 @@ namespace apiHypster.Controllers
     public class UserController : ApiController
     {
         public UserController() { }
-        hypster_tv_DAL.memberManagement memberManager = new hypster_tv_DAL.memberManagement();
-        hypster_tv_DAL.Member member = new hypster_tv_DAL.Member();
+        memberManagement memberManager = new memberManagement();
+        Member member = new Member();
         Resources res = new Resources();
-        //bool test = false;
+        //bool test = true;
         [Route("user/validate")]
         [HttpGet]
         public responseData validate()
         {
-            responseData obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "API error: The requested resource does not support http method 'GET'." }; ;
+            responseData obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "API error: The requested resource does not support http method 'GET'." };
             return obj;
         }
         [Route("user/validate")]
         [HttpPost]
         public responseData validate([FromBody] User user)
         {
-            HttpRequest httpReq = HttpContext.Current.Request;            
+            HttpRequest httpReq = HttpContext.Current.Request;
             responseData obj = new responseData();
+            memberUser mem = new memberUser();
             string clientIp = res.GetUserIP();
             //if (test?true:res.checkIpAddr(clientIp))
             if (res.checkIpAddr(clientIp))
@@ -47,17 +43,7 @@ namespace apiHypster.Controllers
                             obj = new responseData { status = (int)Resources.xhrCode.INVALID, message = "Invalid Password" };
                         else
                         {
-                            memberUser mem = new memberUser {
-                                id = member.id,
-                                username = member.username,
-                                email = member.email,
-                                name = member.name,
-                                country = (member.country != "" ? member.country.ToLower() : null),
-                                city = (member.city != "" ? member.city.ToLower() : null),
-                                zipcode = (member.zipcode != "" ? member.zipcode.ToLower() : null),
-                                birth = Convert.ToDateTime(member.birth).ToString("MM/dd/yyyy"),
-                                adminlevel = member.adminLevel
-                            };
+                            mem = res.padData(member, new memberUser());
                             obj = new responseData { status = (int)Resources.xhrCode.OK, message = "Validation Successful", user = mem };
                         }
                     }
@@ -66,18 +52,12 @@ namespace apiHypster.Controllers
                 }
                 catch (Exception e)
                 {
+                    obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "Internal Server Error during the process" };
                     HttpResponse httpRes = HttpContext.Current.Response;
                     httpRes.Status = "500 Internal Server Error";
                     httpRes.StatusCode = 500;
                     httpRes.StatusDescription = "Internal Server Error during the process";
-                    httpRes.AddHeader("ClientIPAddr", clientIp);
-                    httpRes.AddHeader("Status", "500 Internal Server Error");
-                    httpRes.AddHeader("StatusCode", "500");
-                    httpRes.AddHeader("InnerException", e.InnerException.ToString());
-                    httpRes.AddHeader("Message", e.Message);
-                    httpRes.AddHeader("StackTrace", e.StackTrace);
                     httpRes.Flush();
-                    obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "Internal Server Error during the process" };
                 }
             }
             else
@@ -102,6 +82,7 @@ namespace apiHypster.Controllers
         {
             HttpResponse httpRes = HttpContext.Current.Response;
             responseData obj = new responseData();
+            memberUser mem = new memberUser();
             string clientIp = res.GetUserIP();
             //if (test?true:res.checkIpAddr(clientIp))
             if (res.checkIpAddr(clientIp))
@@ -112,18 +93,7 @@ namespace apiHypster.Controllers
                     member = memberManager.getMemberByID(userId);
                     if (member.id != 0)
                     {
-                        memberUser mem = new memberUser
-                        {
-                            id = member.id,
-                            username = member.username,
-                            email = member.email,
-                            name = member.name,
-                            country = (member.country!=""?member.country.ToLower() : null),
-                            city = (member.city != "" ? member.city.ToLower() : null),
-                            zipcode = (member.zipcode != "" ? member.zipcode.ToLower() : null),
-                            birth = Convert.ToDateTime(member.birth).ToString("MM/dd/yyyy"),
-                            adminlevel = member.adminLevel
-                        };
+                        mem = res.padData(member, new memberUser());
                         obj = new responseData { status = (int)Resources.xhrCode.OK, message = "User Found", user = mem };
                     }
                     else
@@ -135,12 +105,75 @@ namespace apiHypster.Controllers
                     httpRes.Status = "500 Internal Server Error";
                     httpRes.StatusCode = 500;
                     httpRes.StatusDescription = "Internal Server Error during the process";
-                    httpRes.AddHeader("ClientIPAddr", clientIp);
-                    httpRes.AddHeader("Status", "500 Internal Server Error");
-                    httpRes.AddHeader("StatusCode", "500");
-                    httpRes.AddHeader("InnerException", e.InnerException.ToString());
-                    httpRes.AddHeader("Message", e.Message);
-                    httpRes.AddHeader("StackTrace", e.StackTrace);
+                    httpRes.Flush();
+                }
+            }
+            else
+            {
+                obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "Unauthorized Location from the IP Address: " + clientIp };
+                httpRes.ClearHeaders();
+                httpRes.Status = "403 Forbidden";
+                httpRes.StatusCode = 403;
+                httpRes.StatusDescription = "Unauthorized Location";
+                httpRes.AddHeader("ClientIPAddr", clientIp);
+                httpRes.AddHeader("Status", "403 Forbidden");
+                httpRes.AddHeader("StatusCode", "403");
+                httpRes.AddHeader("StatusDescription", "Unauthorized Location from the IP Address: " + clientIp);
+                httpRes.Flush();
+            }
+            return obj;
+        }
+        [Route("user/{userId:int}")]
+        [HttpPut]
+        public responseData putUser([FromUri] int userId, [FromBody] memberUser user)
+        {
+            HttpRequest httpReq = HttpContext.Current.Request;
+            HttpResponse httpRes = HttpContext.Current.Response;
+            memberUser mem = new memberUser();
+            responseData obj = new responseData();
+            string clientIp = res.GetUserIP();
+            string querystream = "";
+            string message = "";
+            //if (test?true:res.checkIpAddr(clientIp))
+            if (res.checkIpAddr(clientIp))
+            {
+                try
+                {
+                    member = memberManager.getMemberByID(userId);
+                    if (member.id != 0)
+                    {                        
+                        if (user != null)
+                        {
+                            HttpContext.Current.Response.AddHeader("ClientIPAddr", clientIp);
+                            user.id = member.id;
+                            mem = res.padData(member, user);
+                            querystream = res.getQueryStream(member, mem);
+                            if (querystream != "")
+                            {
+                                memberManager.UpdateMemberData(user.id, querystream);
+                                member = memberManager.getMemberByID(userId);
+                                mem = res.padData(member, user);
+                                message = "User Updated";
+                            }
+                            else
+                                message = "User Found Without Update";
+                        }
+                        else
+                        {
+                            mem = res.padData(member, new memberUser());
+                            message = "User Found";
+                        }
+                        obj = new responseData { status = (int)Resources.xhrCode.OK, message = message, user = mem };
+                    }
+                    else
+                        obj = new responseData { status = (int)Resources.xhrCode.NOT_FOUND, message = "User Not Found" };
+                }
+                catch (Exception e)
+                {
+                    obj = new responseData { status = (int)Resources.xhrCode.ERROR, message = "Internal Server Error during the process" };
+                    httpRes.Status = "500 Internal Server Error";
+                    httpRes.StatusCode = 500;
+                    httpRes.StatusDescription = "Internal Server Error during the process";
                     httpRes.Flush();
                 }
             }
--- apiHypster/Models/Resources.cs
+++ apiHypster/Models/Resources.cs
@@ -11,6 +11,7 @@ namespace apiHypster.Models
     {
         public enum xhrCode { OK = 1, ERROR, AUTH, INVALID, DUPLICATE, EXPIRED, NOT_FOUND, INCOMPLETE, HUMAN };
         public string[] allowIPAddress = { "66.117.119.138", "23.253.156.54", "127.0.0.1","10.0.0.114" };
+        public List<string> properties = (from t in typeof(hypster_tv_DAL.Member).GetProperties() select t.Name).ToList();
         public string GetUserIP()
         {
             string strIP = String.Empty;
@@ -53,6 +54,75 @@ namespace apiHypster.Models
                     continue;
             }
             return result;
+        }
+
+        public string getQueryStream(hypster_tv_DAL.Member member, memberUser user)
+        {
+            string querystream = "";
+            if (member.username != user.username) querystream += "username='" + user.username + "'";
+            if (member.password != user.password) querystream += ((querystream!="")?",":"") + "password='" + user.password + "'";
+            if (member.email != user.email) querystream += ((querystream != "") ? "," : "") + "email='" + user.email + "'";
+            if (member.name != user.name) querystream += ((querystream != "") ? "," : "") + "name='" + user.name + "'";
+            if (member.country != user.country) querystream += ((querystream != "") ? "," : "") + "country='" + user.country + "'";
+            if (member.city != user.city) querystream += ((querystream != "") ? "," : "") + "city='" + user.city + "'";
+            if (member.zipcode != user.zipcode) querystream += ((querystream != "") ? "," : "") + "zipcode='" + user.zipcode + "'";
+            if (Convert.ToDateTime(member.birth).ToString("MM/dd/yyyy") != Convert.ToDateTime(user.birth).ToString("MM/dd/yyyy")) querystream += ((querystream != "") ? "," : "") + "birth='" + user.birth + "'";
+            if (member.introduce != user.introduce) querystream += ((querystream != "") ? "," : "") + "introduce='" + user.introduce + "'";
+            if (member.sex != user.sex) querystream += ((querystream != "") ? "," : "") + "sex=" + user.sex;
+            if (member.trackcount != user.trackcount) querystream += ((querystream != "") ? "," : "") + "trackcount=" + user.trackcount;
+            if (member.israndomplay != user.israndomplay) querystream += ((querystream != "") ? "," : "") + "israndomplay=" + user.israndomplay;
+            if (member.active_playlist != user.active_playlist) querystream += ((querystream != "") ? "," : "") + "active_playlist=" + user.active_playlist;
+            if (member.profile_pic_id != user.profile_pic_id) querystream += ((querystream != "") ? "," : "") + "profile_pic_id=" + user.profile_pic_id;
+            if (member.is_new_artist != user.is_new_artist) querystream += ((querystream != "") ? "," : "") + "is_new_artist=" + user.is_new_artist;
+            if (member.autoplay != user.autoplay) querystream += ((querystream != "") ? "," : "") + "autoplay=" + user.autoplay;
+            if (member.email_confirmed != user.email_confirmed) querystream += ((querystream != "") ? "," : "") + "email_confirmed=" + user.email_confirmed;
+            if (member.email_optout != user.email_optout) querystream += ((querystream != "") ? "," : "") + "email_optout=" + user.email_optout;
+            if (member.widget_views != user.widget_views) querystream += ((querystream != "") ? "," : "") + "widget_views=" + user.widget_views;
+            if (Convert.ToInt16(member.CoregProcessed) != user.coreg_processed) querystream += ((querystream != "") ? "," : "") + "CoregProcessed=" + user.coreg_processed;
+            if (member.StatusId != user.status_id) querystream += ((querystream != "") ? "," : "") + "StatusId=" + user.status_id;
+            if (Convert.ToInt16(member.AutoshareEnabled) != user.autoshare_enabled) querystream += ((querystream != "") ? "," : "") + "AutoshareEnabled=" + user.autoshare_enabled;
+            if (member.ArtistLevel != user.artist_level) querystream += ((querystream != "") ? "," : "") + "ArtistLevel=" + user.artist_level;
+            if (Convert.ToInt16(member.IsOverEighteen) != user.is_over_eighteen) querystream += ((querystream != "") ? "," : "") + "IsOverEighteen=" + user.is_over_eighteen;
+            if (member.user_interest != user.user_interest) querystream += ((querystream != "") ? "," : "") + "user_interest=" + user.user_interest;
+            if (member.eCampD != user.e_camp_d) querystream += ((querystream != "") ? "," : "") + "eCampD=" + user.e_camp_d;
+            if (member.adminLevel != user.adminlevel) querystream += ((querystream != "") ? "," : "") + "adminLevel=" + user.adminlevel;
+            if (Convert.ToInt16(member.IsSuspended) != user.is_suspended) querystream += ((querystream != "") ? "," : "") + "IsSuspended=" + user.is_suspended;
+            return querystream;
+        }
+
+        public memberUser padData(hypster_tv_DAL.Member member, memberUser user)
+        {
+            memberUser mem = new memberUser();
+            mem.id = ((user.id == 0) ? member.id : user.id);
+            mem.username = (user.username != null ? user.username : member.username);
+            mem.password = (user.password != null ? user.password : member.password);
+            mem.email = (user.email != null ? user.email : member.email);
+            mem.name = (user.name != null ? user.name : member.name);
+            mem.country = (user.country != null ? user.country : member.country);
+            mem.city = (user.city != null ? user.city : member.city);
+            mem.zipcode = (user.zipcode != null ? user.zipcode : member.zipcode);
+            mem.birth = (user.birth != null ? Convert.ToDateTime(user.birth).ToString("MM/dd/yyyy") : Convert.ToDateTime(member.birth).ToString("MM/dd/yyyy"));
+            mem.introduce = (user.introduce != null ? user.introduce : member.introduce);
+            mem.sex = (user.sex != null ? Convert.ToSByte(user.sex) : Convert.ToSByte(member.sex));
+            mem.trackcount = ((user.trackcount != null) ? user.trackcount : member.trackcount);
+            mem.israndomplay = ((user.israndomplay != null) ? user.israndomplay : member.israndomplay);
+            mem.active_playlist = ((user.active_playlist != null) ? user.active_playlist : member.active_playlist);
+            mem.profile_pic_id = ((user.profile_pic_id != null) ? user.profile_pic_id : member.profile_pic_id);
+            mem.is_new_artist = ((user.is_new_artist != null) ? user.is_new_artist : member.is_new_artist);
+            mem.autoplay = ((user.autoplay != null) ? user.autoplay : member.autoplay);
+            mem.email_confirmed = ((user.email_confirmed != null) ? user.email_confirmed : member.email_confirmed);
+            mem.email_optout = ((user.email_optout != null) ? user.email_optout : member.email_optout);
+            mem.widget_views = ((user.widget_views != null) ? user.widget_views : member.widget_views);
+            mem.coreg_processed = ((user.coreg_processed != null) ? user.coreg_processed : Convert.ToInt16(member.CoregProcessed));
+            mem.status_id = ((user.status_id != null) ? user.status_id : member.StatusId);
+            mem.autoshare_enabled = ((user.autoshare_enabled != null) ? user.autoshare_enabled : Convert.ToInt16(member.AutoshareEnabled));
+            mem.artist_level = ((user.artist_level != null) ? user.artist_level : member.ArtistLevel); 
+            mem.is_over_eighteen = ((user.is_over_eighteen != null) ? user.is_over_eighteen : Convert.ToInt16(member.IsOverEighteen)); 
+            mem.user_interest = ((user.user_interest != null) ? user.user_interest : member.user_interest);
+            mem.e_camp_d = ((user.e_camp_d != null) ? user.e_camp_d : member.eCampD);
+            mem.adminlevel = ((user.adminlevel != null) ? user.adminlevel : member.adminLevel);
+            mem.is_suspended = (user.is_suspended != null ? user.is_suspended : Convert.ToInt16(member.IsSuspended));
+            return mem;
         }
     }
 }
