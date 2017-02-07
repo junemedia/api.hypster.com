1f68fd897036f9dfca97efd9f85234789c2ee419
Richard Wu <richardw@junemedia.com>
2017-02-07 17:47:13 -0500

    Take out IP validation from news/feed

--- apiHypster/Controllers/newsController.cs
+++ apiHypster/Controllers/newsController.cs
@@ -22,42 +22,26 @@ namespace apiHypster.Controllers
             newsResponseData obj = new newsResponseData();
             newsPosts[] newsPo = new newsPosts[maxLength];
             string clientIp = res.GetUserIP();
-            if (res.checkIpAddr(clientIp))
+            HttpContext.Current.Response.AddHeader("ClientIPAddr", clientIp);
+            try
             {
-                HttpContext.Current.Response.AddHeader("ClientIPAddr", clientIp);
-                try
+                for (int i = 0; i < maxLength; i++)
                 {
-                    for (int i = 0; i < maxLength; i++)
-                    {
-                        newsPosts news = new newsPosts();
-                        news.href = (newsManager.GetAllNews()[i].post_guid != null && newsManager.GetAllNews()[i].post_guid != "") ? "http://hypster.com/breaking/details/" + newsManager.GetAllNews()[i].post_guid : null;
-                        news.imgsrc = (newsManager.GetAllNews()[i].post_image != null && newsManager.GetAllNews()[i].post_image != "") ? "http://hypster.com/imgs/i_posts/" + newsManager.GetAllNews()[i].post_image : null;
-                        news.title = newsManager.GetAllNews()[i].post_title;
-                        newsPo[i] = news;
-                    }
-                    obj = new newsResponseData { status = (int)Resources.xhrCode.OK, message = "News Listed", content = newsPo };
+                    newsPosts news = new newsPosts();
+                    news.href = (newsManager.GetAllNews()[i].post_guid != null && newsManager.GetAllNews()[i].post_guid != "") ? "http://hypster.com/breaking/details/" + newsManager.GetAllNews()[i].post_guid : null;
+                    news.imgsrc = (newsManager.GetAllNews()[i].post_image != null && newsManager.GetAllNews()[i].post_image != "") ? "http://hypster.com/imgs/i_posts/" + newsManager.GetAllNews()[i].post_image : null;
+                    news.title = newsManager.GetAllNews()[i].post_title;
+                    newsPo[i] = news;
                 }
-                catch (Exception e)
-                {
-                    obj = new newsResponseData { status = (int)Resources.xhrCode.ERROR, message = e.ToString() };
-                    httpRes.Status = "500 Internal Server Error";
-                    httpRes.StatusCode = 500;
-                    httpRes.StatusDescription = "Internal Server Error during the process";
-                    httpRes.Flush();
-                }
+                obj = new newsResponseData { status = (int)Resources.xhrCode.OK, message = "News Listed", content = newsPo };
             }
-            else
+            catch (Exception e)
             {
-                httpRes.ClearHeaders();
-                httpRes.Status = "403 Forbidden";
-                httpRes.StatusCode = 403;
-                httpRes.StatusDescription = "Unauthorized Location";
-                httpRes.AddHeader("ClientIPAddr", clientIp);
-                httpRes.AddHeader("Status", "403 Forbidden");
-                httpRes.AddHeader("StatusCode", "403");
-                httpRes.AddHeader("StatusDescription", "Unauthorized Location from the IP Address: " + clientIp);
+                obj = new newsResponseData { status = (int)Resources.xhrCode.ERROR, message = e.ToString() };
+                httpRes.Status = "500 Internal Server Error";
+                httpRes.StatusCode = 500;
+                httpRes.StatusDescription = "Internal Server Error during the process";
                 httpRes.Flush();
-                obj = new newsResponseData { status = (int)Resources.xhrCode.ERROR, message = "Unauthorized Location from the IP Address: " + clientIp };
             }
             return obj;
         }
