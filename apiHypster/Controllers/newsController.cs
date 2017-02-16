5d6f7ca543724226538eac6a7021f4c57d177a7a
Richard Wu <richardw@junemedia.com>
2017-02-16 17:03:35 -0500

    Latest fixes to the User profiles and API.

--- apiHypster/Controllers/newsController.cs
+++ apiHypster/Controllers/newsController.cs
@@ -43,7 +43,6 @@ namespace apiHypster.Controllers
                 httpRes.StatusCode = 500;
                 httpRes.StatusDescription = "Internal Server Error during the process";
                 httpRes.Flush();
-                httpRes.End();
             }
             return obj;
         }
@@ -64,49 +63,71 @@ namespace apiHypster.Controllers
                 List<sp_postNewsletter_GetPostsByAttribute_Result> Featured = newsManager_admin.GetPostsByAttribute(0, "Featured");
                 featuredObj newest = new featuredObj();
                 item newest_feature = new item();
-                newest_feature.href = (Featured[0].post_guid != null && Featured[0].post_guid != "") ? "http://hypster.com/breaking/details/" + Featured[0].post_guid : null;
-                newest_feature.imgsrc = (Featured[0].post_image != null && Featured[0].post_image != "") ? "http://hypster.com/imgs/i_posts/" + Featured[0].post_image : null;
-                newest_feature.title = Featured[0].post_title;
-                newest.featured = newest_feature;
-                int exclude_id = Featured[0].post_id;
-                List<sp_postNewsletter_GetPostsByAttribute_Result> Newsletter = newsManager_admin.GetPostsByAttribute(exclude_id, "Newsletter");
-                newest.items = new item[Newsletter.Count];
-                for (int i = 0; i < Newsletter.Count; i++)
+                int exclude_id = 0;
+                if (Featured.Count > 0)
                 {
-                    item newest_newsletter = new item();
-                    newest_newsletter.href = (Newsletter[i].post_guid != null && Newsletter[i].post_guid != "") ? "http://hypster.com/breaking/details/" + Newsletter[i].post_guid : null;
-                    newest_newsletter.imgsrc = (Newsletter[i].post_image != null && Newsletter[i].post_image != "") ? "http://hypster.com/imgs/i_posts/" + Newsletter[i].post_image : null;
-                    newest_newsletter.title = Newsletter[i].post_title;
-                    newest.items[i] = newest_newsletter;
+                    newest_feature.href = (Featured[0].post_guid != null && Featured[0].post_guid != "") ? "http://hypster.com/breaking/details/" + Featured[0].post_guid : null;
+                    newest_feature.imgsrc = (Featured[0].post_image != null && Featured[0].post_image != "") ? "http://hypster.com/imgs/i_posts/" + Featured[0].post_image : null;
+                    newest_feature.title = Featured[0].post_title;
+                    exclude_id = Featured[0].post_id;
                 }
+                newest.featured = newest_feature;                
+                List<sp_postNewsletter_GetPostsByAttribute_Result> Newsletter = newsManager_admin.GetPostsByAttribute(exclude_id, "Newsletter");
+                if (Newsletter.Count > 0)
+                {
+                    newest.items = new item[Newsletter.Count];
+                    for (int i = 0; i < Newsletter.Count; i++)
+                    {
+                        item newest_newsletter = new item();
+                        newest_newsletter.href = (Newsletter[i].post_guid != null && Newsletter[i].post_guid != "") ? "http://hypster.com/breaking/details/" + Newsletter[i].post_guid : null;
+                        newest_newsletter.imgsrc = (Newsletter[i].post_image != null && Newsletter[i].post_image != "") ? "http://hypster.com/imgs/i_posts/" + Newsletter[i].post_image : null;
+                        newest_newsletter.title = Newsletter[i].post_title;
+                        newest.items[i] = newest_newsletter;
+                    }
+                }
+                else
+                    newest.items = null;
                 cont.newest = newest;
                 featuredObj playlists = new featuredObj();
                 item playlists_feature = new item();
                 List<sp_postNewsletter_GetPostsByAttribute_Result> Playlist = newsManager_admin.GetPostsByAttribute(0, "Playlist");
-                playlists.items = new item[Playlist.Count-1];
-                playlists_feature.href = (Playlist[0].post_guid != null && Playlist[0].post_guid != "") ? "http://hypster.com/breaking/details/" + Playlist[0].post_guid : null;
-                playlists_feature.imgsrc = (Playlist[0].post_image != null && Playlist[0].post_image != "") ? "http://hypster.com/imgs/i_posts/" + Playlist[0].post_image : null;
-                playlists_feature.title = Playlist[0].post_title;
-                playlists.featured = playlists_feature;
-                for (int j = 1; j <= Playlist.Count-1; j++)
+                if (Playlist.Count > 0)
                 {
-                    item playlist_item = new item();
-                    playlist_item.href = (Playlist[j].post_guid != null && Playlist[j].post_guid != "") ? "http://hypster.com/breaking/details/" + Playlist[j].post_guid : null;
-                    playlist_item.imgsrc = (Playlist[j].post_image != null && Playlist[j].post_image != "") ? "http://hypster.com/imgs/i_posts/" + Playlist[j].post_image : null;
-                    playlist_item.title = Playlist[j].post_title;
-                    playlists.items[j-1] = playlist_item;
+                    playlists.items = new item[Playlist.Count - 1];
+                    playlists_feature.href = (Playlist[0].post_guid != null && Playlist[0].post_guid != "") ? "http://hypster.com/breaking/details/" + Playlist[0].post_guid : null;
+                    playlists_feature.imgsrc = (Playlist[0].post_image != null && Playlist[0].post_image != "") ? "http://hypster.com/imgs/i_posts/" + Playlist[0].post_image : null;
+                    playlists_feature.title = Playlist[0].post_title;
+                    playlists.featured = playlists_feature;
+                    for (int j = 1; j <= Playlist.Count - 1; j++)
+                    {
+                        item playlist_item = new item();
+                        playlist_item.href = (Playlist[j].post_guid != null && Playlist[j].post_guid != "") ? "http://hypster.com/breaking/details/" + Playlist[j].post_guid : null;
+                        playlist_item.imgsrc = (Playlist[j].post_image != null && Playlist[j].post_image != "") ? "http://hypster.com/imgs/i_posts/" + Playlist[j].post_image : null;
+                        playlist_item.title = Playlist[j].post_title;
+                        playlists.items[j - 1] = playlist_item;
+                    }
+                }
+                else
+                {
+                    playlists.featured = null;
+                    playlists.items = null;
                 }
                 cont.playlists = playlists;
                 List<sp_postNewsletter_GetPostsByAttribute_Result> More = newsManager_admin.GetPostsByAttribute(0, "More");
-                cont.more = new item[More.Count];
-                for (int k = 0; k < More.Count; k++)
+                if (More.Count > 0)
                 {
-                    item more_item = new item();
-                    more_item.href = (More[k].post_guid != null && More[k].post_guid != "") ? "http://hypster.com/breaking/details/" + More[k].post_guid : null;
-                    more_item.imgsrc = (More[k].post_image != null && More[k].post_image != "") ? "http://hypster.com/imgs/i_posts/" + More[k].post_image : null;
-                    more_item.title = More[k].post_title;
-                    cont.more[k] = more_item;
+                    cont.more = new item[More.Count];
+                    for (int k = 0; k < More.Count; k++)
+                    {
+                        item more_item = new item();
+                        more_item.href = (More[k].post_guid != null && More[k].post_guid != "") ? "http://hypster.com/breaking/details/" + More[k].post_guid : null;
+                        more_item.imgsrc = (More[k].post_image != null && More[k].post_image != "") ? "http://hypster.com/imgs/i_posts/" + More[k].post_image : null;
+                        more_item.title = More[k].post_title;
+                        cont.more[k] = more_item;
+                    }
                 }
+                else
+                    cont.more = null;
                 obj = new newsFeaturedData { status = (int)Resources.xhrCode.OK, message = "Success", content = cont };
             }
             catch (Exception e)
